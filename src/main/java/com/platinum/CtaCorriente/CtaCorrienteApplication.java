2b632e54b1735b80752f189390de18ec60cba456
ynaveac <yencar1982@gmail.com>
2022-07-04 17:27:11 -0400

    CtaCorriene
    CtaCorriente

---
+++ src/main/java/com/platinum/CtaCorriente/CtaCorrienteApplication.java
@@ -0,0 +1,13 @@
+package com.platinum.CtaCorriente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtaCorrienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CtaCorrienteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/platinum/CtaCorriente/CtaCorrienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platinum.CtaCorriente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtaCorrienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
